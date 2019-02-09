ce4c7e728da7b470dc03fa251e96c9eb96a940de
Kurtis Fields <kurtisfields@gmail.com>
2019-02-09 00:04:00 -0500

    Navbar fix

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,6 @@
 <template>
   <header>
-    <nav class="navbar is-fixed-top" role="navigation" aria-label="main navigation">
+    <nav class="navbar" role="navigation" aria-label="main navigation">
       <div class="navbar-brand">
         <a class="navbar-item" href="/">
           <img alt class="nav-logo" src="/logos/logo-circle-normal.svg">
@@ -13,6 +13,7 @@
           aria-label="menu"
           aria-expanded="false"
           data-target="navbarBasicExample"
+          @click="burgerClick"
         >
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
@@ -20,7 +21,7 @@
         </a>
       </div>
 
-      <div id="navbarBasicExample" class="navbar-menu">
+      <div id="navbarBasicExample" class="navbar-menu" ref="navMenu">
         <div class="navbar-start">
           <g-link class="navbar-item" to="/about">About</g-link>
           <g-link class="navbar-item" to="/contact">Contact</g-link>
@@ -59,26 +60,18 @@ export default {
     return {};
   },
   mounted: function() {
-    // Get all "navbar-burger" elements
-    const $navbarBurgers = Array.prototype.slice.call(
-      document.querySelectorAll(".navbar-burger"),
-      0
-    );
+  },
+  methods: {
+    burgerClick (event) {
+      const el = event.currentTarget
+      // Get the target from the "data-target" attribute
+      const target = el.dataset.target;
+      //const $target = document.getElementById(target);
+      const $target = this.$refs['navMenu']
 
-    // Check if there are any navbar burgers
-    if ($navbarBurgers.length > 0) {
-      // Add a click event on each of them
-      $navbarBurgers.forEach(el => {
-        el.addEventListener("click", () => {
-          // Get the target from the "data-target" attribute
-          const target = el.dataset.target;
-          const $target = document.getElementById(target);
-
-          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-          el.classList.toggle("is-active");
-          $target.classList.toggle("is-active");
-        });
-      });
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      el.classList.toggle("is-active");
+      $target.classList.toggle("is-active");
     }
   }
 };
